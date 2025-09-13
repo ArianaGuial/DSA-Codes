9eb6759b22ce1c1ea54731229f99f6d02106162a
Ariana <guialmariearianalexi@gmial.com>
2025-09-13 11:08:41 +0800

    13th commit

---
+++ CodeChum/CODECHUM1.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 5
+
+typedef struct 
+{
+    int items[MAX];
+    int top;
+} Stack;
+
+// write your functions here
+
+void initialize(Stack *s)
+{
+    s->top = -1;
+}
+
+bool isFull(Stack *s)
+{
+    return s->top == MAX -1;
+}
+
+bool isEmpty(Stack *s)
+{
+    return s->top == -1;
+}
+
+void push(Stack *s, int value)
+{
+    if(!isFull(s))
+    {
+        s->items[++(s->top)] = value;
+    }
+}
+
+int pop(Stack *s)
+{
+    if(!isEmpty(s))
+    {
+        return s->items[(s->top)--];
+    }
+    
+    return -1;
+}
+
+int peek(Stack *s)
+{
+    if(!isEmpty(s))
+    {
+        return s->items[s->top];
+    }
+    
+    return -1;
+}
+
+int top(Stack *s)
+{
+    return s->top;
+}
+
+void display(Stack* s)
+{
+    if(isEmpty(s))
+    {
+        printf("Stack is empty.");
+    }
+    
+    else
+    {
+        for(int i = s->top; i >= 0; i--)
+        {
+            printf("%d\n", s->items[i]);
+        }
+        printf("\b");
+    }
+}
+
+int main() 
+{
+    // write your code here
+
+    Stack stacks[3];
+    int i, choice, containerID;
+    bool found;
+
+    for(i = 0; i < 3; i++) 
+    {
+        initialize(&stacks[i]);
+    }
+
+    while (1) 
+    {
+        printf("\n--- Warehouse Inventory Management System ---\n");
+        printf("1. Push a container\n");
+        printf("2. Pop a container\n");
+        printf("3. Check if a container exists\n");
+        printf("4. Display all stacks\n");
+        printf("5. Exit\n");
+        
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) 
+        {
+            case 1:
+                printf("Enter container ID to push: ");
+                scanf("%d", &containerID);
+
+                for(i = 0; i < 3; i++)
+                {
+                    if (!isFull(&stacks[i])) 
+                    {
+                        push(&stacks[i], containerID);
+                        printf("Container %d pushed successfully.\n", containerID);
+                        break;
+                    }
+                }
+                
+                if(i == 3) 
+                {
+                    printf("All stacks are full. Cannot push container.\n");
+                }
+                
+                break;
+
+            case 2:
+                for(i = 2; i >= 0; i--) 
+                {
+                    if (!isEmpty(&stacks[i])) 
+                    {
+                        int removed = pop(&stacks[i]);
+                        printf("Container %d popped successfully.\n", removed);
+                        break;
+                    }
+                }
+                
+                if(i < 0)
+                {
+                    printf("Error: All stacks are empty. Cannot pop container.\n");
+                }
+                
+                break;
+
+            case 3:
+                printf("Enter container ID to check: ");
+                scanf("%d", &containerID);
+                found = false;
+
+                for(i = 0; i < 3; i++) 
+                {
+                    for(int j = 0; j <= stacks[i].top; j++) 
+                    {
+                        if(stacks[i].items[j] == containerID) 
+                        {
+                            printf("Container %d exists in a stack.\n", containerID);
+                            found = true;
+                        }
+                    }
+                }
+                
+                if(!found) 
+                {
+                    printf("Container %d does not exist in any stack.\n", containerID);
+                }
+                break;
+
+            case 4:
+                for(i = 0; i < 3; i++) 
+                {
+                    printf("\n--- Stack %d ---\n", i + 1);
+                    display(&stacks[i]);
+                    
+                    printf("\n");
+                }
+                
+                break;
+
+            case 5:
+                printf("Exiting program.");
+                exit(0);
+
+            default:
+                printf("Invalid.\n");
+        }
+    }
+
+    return 0;
+}
---
+++ CodeChum/CODECHUM2.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 10
+
+typedef struct 
+{
+    int items[MAX];
+    int count;
+} List;
+
+typedef struct 
+{
+    List list;
+    int front;
+    int rear;
+} Queue;
+
+// write your functions here
+
+Queue* initialize()
+{
+    Queue* q = (Queue *)malloc(sizeof(Queue));
+    q->front = 0;
+    q->rear = -1;
+    q->list.count = 0;
+    
+    return q;
+}
+
+void enqueue(Queue* q, int value)
+{
+    if(q->list.count == MAX)
+    {
+        printf("Queue is full.\n");
+        return;
+    }
+    
+    q->rear = (q->rear + 1) % MAX;
+    q->list.items[q->rear] = value;
+    q->list.count++;
+}
+
+int dequeue(Queue* q)
+{
+    bool isEmpty(Queue* q);
+    
+    if(isEmpty(q))
+    {
+        return -1;
+    }
+    
+    int value = q->list.items[q->front];
+    q->front = (q->front + 1) % MAX;
+    q->list.count--;
+    
+    return value;
+}
+
+bool isFull(Queue* q)
+{
+    return q->list.count == MAX;
+}
+
+bool isEmpty(Queue* q)
+{
+    return q->list.count == 0;
+}
+
+void display(Queue* q)
+{
+    if(isEmpty(q))
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+    
+    int i;
+    
+    for(i = 0; i < q->list.count; i++)
+    {
+        int index = (q->front + i) % MAX;
+        printf("%d ", q->list.items[index]);
+    }
+    
+    printf("\n");
+}
+
+int main() 
+{
+    // write your code here
+    
+    Queue* regularQueue = initialize();
+    Queue* priorityQueue = initialize();
+            
+    int customerNumber = 1;
+    int choice;
+            
+    while(1)
+    {
+        printf("\n--- Pharmacy Queueing System ---\n");
+        printf("1. Enter Regular Queue\n");
+        printf("2. Enter Priority Queue\n");
+        printf("3. Call Next Customer\n");
+        printf("4. Display Queues\n");
+        printf("5. Exit\n");
+        
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice)
+        {
+            case 1:
+                if(isFull(regularQueue))
+                {
+                   printf("Regular queue is full. Please try again later.\n");
+                }
+                else
+                {
+                    enqueue(regularQueue, customerNumber);
+                    printf("You have entered the Regular queue. Your number is %d.\n", customerNumber);
+                    customerNumber++;
+                }
+                break;
+                
+            case 2:
+                if(isFull(priorityQueue))
+                {
+                    printf("Priority queue is full. Please try again later.\n");
+                }
+                else
+                {
+                    enqueue(priorityQueue, customerNumber);
+                    printf("You have entered the Priority queue. Your number is %d.\n", customerNumber);
+                    customerNumber++;
+                }
+
+                break;
+            
+            case 3:
+                if(!isEmpty(priorityQueue))
+                {
+                    int served = dequeue(priorityQueue);
+                    printf("Calling next customer from Priority Queue. Customer number: %d\n", served);
+                }
+                else if(!isEmpty(regularQueue))
+                {
+                    int served = dequeue(regularQueue);
+                    printf("Calling next customer from Regular Queue. Customer number: %d\n", served);
+                }
+                else
+                {
+                    printf("No customers in queue.\n");
+                }
+                
+                break;
+            
+            case 4:
+                printf("\n-- Current Queue Status --\n");
+                
+                printf("Regular Queue: ");
+                display(regularQueue);
+                
+                printf("Priority Queue: ");
+                display(priorityQueue);
+                
+                break;
+            
+            case 5:
+                printf("Exiting program. Goodbye!\n");
+                free(regularQueue);
+                free(priorityQueue);
+                return 0;
+                
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    
+    return 0;
+}
